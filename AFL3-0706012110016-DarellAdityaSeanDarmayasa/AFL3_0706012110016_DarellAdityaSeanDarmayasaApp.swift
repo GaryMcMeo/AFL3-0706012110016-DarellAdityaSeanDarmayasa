5b608214445afb79ec4f079979a8d381be0bcb64
GaryMcMeo <adityadarmayasa@gmail.com>
2023-04-18 08:28:52 +0700

    Initial Commit

---
+++ AFL3-0706012110016-DarellAdityaSeanDarmayasa/AFL3_0706012110016_DarellAdityaSeanDarmayasaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AFL3_0706012110016_DarellAdityaSeanDarmayasaApp.swift
+//  AFL3-0706012110016-DarellAdityaSeanDarmayasa
+//
+//  Created by MacBook Pro on 18/04/23.
+//
+
+import SwiftUI
+
+@main
+struct AFL3_0706012110016_DarellAdityaSeanDarmayasaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
